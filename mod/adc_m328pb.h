55f08f5d462d140317421e31a4cbdfdd0d10f50f
Piotr Michna <pm@piotrmichna.pl>
2020-08-30 22:02:42 +0200

    definicje preprocesora biblioteki ACD

--- mod/adc_m328pb.h
+++ mod/adc_m328pb.h
@@ -10,7 +10,18 @@
 #ifndef ADC_M328PB_H_
 #define ADC_M328PB_H_
 
+#define ADC_SLEEP_MODE 1
+//#define ADC_REFS 0		// AREF, zewnêtrzne napiecie odniesienia
+//#define ADC_REFS 64		// AVcc
+#define ADC_REFS 192	// Internal 1.1V
 
+//#define ADC_PRESCALER 1		//2
+//#define ADC_PRESCALER 2		//4
+//#define ADC_PRESCALER 3		//8
+//#define ADC_PRESCALER 4		//16
+//#define ADC_PRESCALER 5		//32
+//#define ADC_PRESCALER 6		//64
+#define ADC_PRESCALER 7		//128
 
 
 
