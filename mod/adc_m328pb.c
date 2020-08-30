a587672f511a0ead1304a43af2b4ba0b9bbb7385
Piotr Michna <pm@piotrmichna.pl>
2020-08-30 22:12:44 +0200

    dolaczenie plikow naglowkowych dla biblioteki ADC

--- mod/adc_m328pb.c
+++ mod/adc_m328pb.c
@@ -5,3 +5,15 @@
   * Author : Piotr Michna
   * e-mail : pm@piotrmichna.pl
  */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "adc_m328pb.h"
+#if ADC_SLEEP_MODE == 1
+	#include <util/atomic.h>
+	#include <avr/sleep.h>
+#else
+	extern volatile uint16_t adc_res;
+	extern volatile uint8_t adc_flag;
+#endif
