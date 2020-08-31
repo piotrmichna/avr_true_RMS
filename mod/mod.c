b6416e5085faa0d79a9619e7e3388de7916d9f36
Piotr Michna <pm@piotrmichna.pl>
2020-08-31 12:46:13 +0200

    rozbudowa funkcji sterowania modulami

--- mod/mod.c
+++ mod/mod.c
@@ -12,6 +12,7 @@
 #include "mod_io.h"
 #include "fnc_ret.h"
 #include "adc_m328pb.h"
+#include "mod.h"
 
 void mod_init(void){
 	mod_io_init();
@@ -23,43 +24,77 @@ uint8_t get_pwr_stan(void){
 uint8_t get_det_stan(void){
 	return mcnf.det_on_f;
 }
+uint8_t get_mod_f(void){
+	return mcnf.mod_f;
+}
 
 int8_t mod_set_on(uint8_t xmod){
 	if( !(mcnf.mod_f & (1<<xmod)) ){
-		if(!mcnf.pwr_on_f) return mod_pwr_set(1);
-
+		if(!mcnf.pwr_on_f){
+			int8_t err;
+			err=mod_pwr_set(1);
+			if(err) return err;
+		}
 		if(mod[xmod].mtk){
 			if(!mod[xmod].mtk_f){
-				mod[xmod].mtk(1);
-				mcnf.mod_f |= (1<<xmod);
-				return F_OK;
+				mod[xmod].mtk(1);				
+				return F_ON_PROGRES;
 			}
-			}else{
-			return F_BRAK_DEF;
 		}
-		}else{
+		if(mod[xmod].mpk){
+			if(!mod[xmod].mpk_f){
+				mod[xmod].mpk(1);
+				return F_ON_PROGRES;				
+			}
+		}
+		if(mod[xmod].ena && mod[xmod].ena_f==0){
+			if(!mcnf.adc_cnt){
+				mod[xmod].ena(1);
+				mcnf.adc_cnt++;
+				return F_ON_PROGRES;
+			}else{
+				if(mcnf.adc_cnt==mcnf.adc_delay){
+					mod[xmod].ena_f=1;
+				}else{
+					mcnf.adc_cnt++;
+					return F_ON_PROGRES;
+				}
+			}
+		}
+		mcnf.mod_f |= (1<<xmod);
+		return F_OK;
+	}else{
 		return F_EMPTY_COLL;
 	}
 }
 
 int8_t mod_set_off(uint8_t xmod){
-	if( mcnf.mod_f & (1<<xmod) ){
-		if(mod[xmod].mtk){
-			if(mod[xmod].mtk_f){
-				mod[xmod].mtk(0);
-				mcnf.mod_f &= ~(1<<xmod);
-				return F_ON_PROGRES;
-			}
-			}else{
-			return F_BRAK_DEF;
+	if( (mcnf.mod_f & (1<<xmod)) || mcnf.pwr_on_f ){	
+		if(mod[xmod].ena && mod[xmod].ena_f==1){
+			mod[xmod].ena(0);
+			mcnf.adc_cnt=0;
+			mod[xmod].ena_f=0;
+			return F_ON_PROGRES;
+		}	
+		if(mod[xmod].mpk_f){
+			if(mod[xmod].mpk) mod[xmod].mpk(0);
+			return F_ON_PROGRES;
+		}
+		if(mod[xmod].mtk_f){
+			if(mod[xmod].mtk) mod[xmod].mtk(0);
+			mcnf.mod_f &= ~(1<<xmod);
+			return F_ON_PROGRES;			
 		}
 		
-		if(mcnf.pwr_on_f){
-			if(mcnf.mod_f==0) return mod_pwr_set(0); else return F_OK;
-			}else{
-			return F_OK;
+		if(mcnf.pwr_on_f){			
+			if(mcnf.mod_f==0) {
+				int8_t err=mod_pwr_set(0); 
+				if(err) return err;
+			}
 		}
-		}else{
+		
+		return F_OK;
+	}else{
 		return F_EMPTY_COLL;
 	}
 }
