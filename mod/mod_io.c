32921f918bae506efbe9a8dbf2c4f7ba6aee4a37
Piotr Michna <pm@piotrmichna.pl>
2020-08-31 06:43:08 +0200

    utworzenie plikow biblioteki modi_io

---
+++ mod/mod_io.c
@@ -0,0 +1,7 @@
+/*
+ * mod_io.c
+ *
+ * Created: 31.08.2020 06:41:20
+ * Author : Piotr Michna
+ * e-mail : pm@piotrmichna.pl
+ */ 
---
+++ mod/mod_io.h
@@ -0,0 +1,17 @@
+/*
+ * mod_io.h
+ *
+ * Created: 31.08.2020 06:39:38
+ * Author : Piotr Michna
+ * e-mail : pm@piotrmichna.pl
+ */ 
+
+
+#ifndef MOD_IO_H_
+#define MOD_IO_H_
+
+
+
+
+
+#endif /* MOD_IO_H_ */
