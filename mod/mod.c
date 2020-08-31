037210b39d95ac2f1d3b70cc00f17bc1b5caaf00
Piotr Michna <pm@piotrmichna.pl>
2020-08-31 09:17:30 +0200

    utworzenie glownych plikow biblioteki

---
+++ mod/mod.c
@@ -0,0 +1,7 @@
+/*
+ * mod.c
+ *
+ * Created: 31.08.2020 09:16:11
+  * Author : Piotr Michna
+  * e-mail : pm@piotrmichna.pl
+ */ 
---
+++ mod/mod.h
@@ -0,0 +1,17 @@
+/*
+ * mod.h
+ *
+ * Created: 31.08.2020 09:14:54
+ * Author : Piotr Michna
+ * e-mail : pm@piotrmichna.pl
+ */ 
+
+
+#ifndef MOD_H_
+#define MOD_H_
+
+
+
+
+
+#endif /* MOD_H_ */
